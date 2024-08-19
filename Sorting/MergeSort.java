881a574e7161075a1c807b4094f891370ff7d5f4
Aditya Dhiman <info@adityadhiman.in>
2024-08-19 20:43:39 +0530

    added merge sort

---
+++ Sorting/MergeSort.java
@@ -0,0 +1,15 @@
+package Sorting;
+
+/**
+ * MergeSort
+ */
+public class MergeSort {
+    public static void divide(int[] arr) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 7, 3, 2, 5, 6, 6, 22, 45 };
+        int len = arr.length;
+    }
+}
