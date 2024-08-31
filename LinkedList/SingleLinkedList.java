0a636df582947ee53f57699b30f2ffdad6946575
Aditya Dhiman <info@adityadhiman.in>
2024-08-31 11:13:30 +0530

    added linked list - single linked list -> completed

--- LinkedList/SingleLinkedList.java
+++ LinkedList/SingleLinkedList.java
@@ -4,6 +4,13 @@ package LinkedList;
  * SingleLinkedList
  */
 public class SingleLinkedList {
+    Node head;
+    private int size;
+
+    SingleLinkedList() {
+        this.size = 0;
+    }
+
     class Node {
         String data;
         Node next;
@@ -11,10 +18,11 @@ public class SingleLinkedList {
         Node(String data) {
             this.data = data;
             this.next = null;
+            size++;
         }
     }
 
-    // add - first , last
+    // Add first
     public void addFirst(String data) {
         Node newNode = new Node(data);
         if (head == null) {
@@ -25,8 +33,77 @@ public class SingleLinkedList {
         head = newNode;
     }
 
+    // Add last
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+
+    // Print
+    public void printList() {
+        if (head == null) {
+            System.out.println("list is empty");
+            return;
+        }
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("Null");
+    }
+
+    // delete first
+    public void deleteFirst() {
+        if (head == null) {
+            System.out.println("linked list is empty");
+            return;
+        }
+        size--;
+        head = head.next;
+    }
+
+    public void deleteLast() {
+        if (head == null) {
+            System.out.println("linked list is empty");
+            return;
+        }
+        size--;
+        if (head.next == null) {
+            head = null;
+            return;
+        }
+        Node secondLast = head;
+        Node lastNode = head.next;
+        while (lastNode.next != null) {
+            lastNode = lastNode.next;
+            secondLast = secondLast.next;
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
     public static void main(String[] args) {
         SingleLinkedList list = new SingleLinkedList();
-
+        list.addFirst("a");
+        list.addFirst("b");
+        list.addLast("c");
+        list.printList();
+        list.deleteFirst();
+        list.printList();
+        list.deleteFirst();
+        list.printList();
+        System.out.println(list.getSize());
     }
+
 }
