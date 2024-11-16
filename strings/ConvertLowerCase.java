d674bd1b498faf47495bfcb724407e3d2b8b5b66
Aditya Dhiman <info@adityadhiman.in>
2024-11-16 15:58:49 +0530

    changes done in solutions

--- strings/ConvertLowerCase.java
+++ strings/ConvertLowerCase.java
@@ -1,13 +1,17 @@
 package strings;
 
 public class ConvertLowerCase {
-    public static void main(String[] args) {
-        String str = "HEllo World";
+    public static void toLowerCaseConverter(String str) {
         for (char ch : str.toCharArray()) {
             if (ch >= 'A' && ch <= 'Z') {
                 ch = (char) (ch + 32);
             }
             System.out.print(ch);
         }
+    }
+
+    public static void main(String[] args) {
+        String str = "HEllo World";
+        toLowerCaseConverter(str);
     }
 }
