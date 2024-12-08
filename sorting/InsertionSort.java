3cfd5e7b639cb8e08a2f135ae5e5275a37e33179
Aditya Dhiman <info@adityadhiman.in>
2024-12-08 19:49:39 +0530

    added insertionSort

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -1,5 +1,23 @@
 class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+        System.out.println("Sorted array is:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = { 5, 2, 8, 1, 9 };
+        insertionSort(arr);
     }
 }
